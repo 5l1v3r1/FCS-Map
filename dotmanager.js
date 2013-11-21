a6733f3c2ab50ccd535989656fcf27256b61399e
Alex Nichol <aqnichol@li550-95.members.linode.com>
2013-11-21 12:27:24 -0500

    made everything a canvas

--- dotmanager.js
+++ dotmanager.js
@@ -1,6 +1,5 @@
-function Dot(sequence, div, blurb, loc) {
-    this.sequence = sequence;
-    this.div = div;
+function Dot(canvas, blurb, loc) {
+    this.canvas = canvas;
     this.blurb = blurb;
     this.loc = loc;
 }
@@ -8,33 +7,34 @@ function Dot(sequence, div, blurb, loc) {
 Dot.prototype.positionOnMap = function(aMap) {
     var pos = aMap.pagePoint(this.loc);
     var scale = aMap.getScale();
-    this.sequence.dimensions = [scale * 7, scale * 7];
-    var xVal = pos[0] - this.sequence.dimensions[0] / 2;
-    var yVal = pos[1] - this.sequence.dimensions[1] / 2;
-    $(this.div).css({'left': Math.round(xVal) + 'px',
+    var size = scale * 7;
+    var xVal = pos[0] - size / 2;
+    var yVal = pos[1] - size / 2;
+    $(this.canvas).css({'left': Math.round(xVal) + 'px',
                      'top': Math.round(yVal) + 'px',
                      'position': 'fixed'});
+    this.canvas.width = size;
+    this.canvas.height = size;
     // setup blurb
-    $(this.blurb).css({'font-size': Math.round(this.sequence.dimensions[0] * 0.6) + 'px'});
-    $(this.blurb).css({'left': Math.round(xVal + this.sequence.dimensions[0]) + 'px',
+    $(this.blurb).css({'font-size': Math.round(size * 0.6) + 'px'});
+    $(this.blurb).css({'left': Math.round(xVal + size) + 'px',
                        'top': Math.round(pos[1] - $(this.blurb).innerHeight() / 2) + 'px',
                        'position': 'fixed',
-                       'text-shadow': '0px 0px ' + Math.round(this.sequence.dimensions[0] / 2) + 'px rgba(0, 0, 0, 0.7)'});
+                       'text-shadow': '0px 0px ' + Math.round(size / 2) + 'px rgba(0, 0, 0, 0.7)'});
 };
 
 function DotManager(aMap) {
     this.map = aMap;
     this.dots = [];
+    this.sequence = new Sequence('BlueDot_Flat', 90, 'BlueDot_', [40, 40]);
+    this.sequence.startAnimating();
 }
 
 DotManager.prototype.addDot = function(loc, html) {
-    var sequence = new Sequence('BlueDot_Flat', 90, 'BlueDot_', [60, 60]);
-
     // create dot div
-    var div = document.createElement('div');
-    sequence.attachToDiv(div);
-    sequence.startAnimating();
-    $('#dotdiv').append($(div));
+    var canvas = document.createElement('canvas');
+    $('#dotdiv').append($(canvas));
+    this.sequence.addCanvas(canvas);
 
     // create blurb div
     var blurb = document.createElement('div');
@@ -42,7 +42,7 @@ DotManager.prototype.addDot = function(loc, html) {
     $(blurb).css({'color': '#0000FF', 'font-family': 'Arial'});
     $('#dotdiv').append($(blurb));
 
-    var dot = new Dot(sequence, div, blurb, loc);
+    var dot = new Dot(canvas, blurb, loc);
     this.dots.push(dot);
     dot.positionOnMap(this.map);
     return dot;
--- sequence.js
+++ sequence.js
@@ -9,31 +9,44 @@ function Sequence(dir, count, prefix, dimensions) {
         images.push(image);
     }
     this.images = images;
-    this.animateDiv = null;
     this.index = 0;
     this.animateId = 0;
     this.dimensions = dimensions;
+    this.canvases = [];
+    this.isRunning = false;
 }
 
-Sequence.prototype.attachToDiv = function(div) {
-    this.animateDiv = div;
+Sequence.prototype.addCanvas = function(canvas) {
+    this.canvases.push(canvas);
+};
+
+Sequence.prototype.removeCanvas = function(canvas) {
+    for (var i = 0; i < this.canvases.length; i++) {
+        if (this.canvases[i] == canvas) return this.canvases.splice(i, 1);
+    }
 };
 
 Sequence.prototype.startAnimating = function() {
+    if (this.isRunning) return;
+    this.isRunning = true;
     this.tick(++this.animateId);
 };
 
 Sequence.prototype.stopAnimating = function() {
     this.animateId++;
+    this.isRunning = false;
 };
 
 Sequence.prototype.tick = function(anId) {
     if (anId != this.animateId) return;
-    var obj = $(this.animateDiv);
-    obj.empty();
-    obj.append($(this.images[this.index++]));
-    this.images[this.index - 1].width = this.dimensions[0];
-    this.images[this.index - 1].height = this.dimensions[1];
+    var image = this.images[this.index++];
+    for (var i = 0; i < this.canvases.length; i++) {
+        var ctx = this.canvases[i].getContext('2d');
+        var width = this.canvases[i].width;
+        var height = this.canvases[i].height;
+        ctx.clearRect(0, 0, width, height);
+        ctx.drawImage(image, 0, 0, width, height);
+    }
     setTimeout(this.tick.bind(this, ++this.animateId), 50);
     if (this.index >= this.images.length) this.index = 0;
 };
